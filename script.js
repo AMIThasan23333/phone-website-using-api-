747ee0a19358c3cc57ac4f870ab0f8c5896fbe56
My Name <cpgsom819@gmail.com>
2023-02-18 03:28:45 +0800

    adding phone details

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
 
 
-const loadPhones =  async(searchText) => {
+const loadPhones =  async(searchText, dataLimit) => {
 
 
-    const url = ` https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
 
     const res = await fetch(url);
 
     const data = await res.json();
 
-    displayPhones (data.data);
+    displayPhones (data.data, dataLimit);
 
 
 }
  
-   const displayPhones = phones => {
+   const displayPhones = (phones, dataLimit) => {
+    const ShowAll = document.getElementById('show-all');
+     if(phones.length > 10 && dataLimit){
 
- 
-     phones = phones.slice(0,10)
-
+        phones = phones.slice(0,10);
+        ShowAll.classList.remove('d-none')
+     } 
+       else{
+        ShowAll.classList.add('d-none')
+       }
     const phoneContainer = document.getElementById('phone-container');
    const  noPhone = document.getElementById('no-phone')
 
@@ -51,6 +56,7 @@ const loadPhones =  async(searchText) => {
         <div class="card-body">
           <h5 class="card-title">${phone.phone_name}</h5>
           <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          <button onclick="loadPhoneDetails('${phone.slug}')" class="btn btn-primary">Go SomeWhere </button>
         </div>
       </div>
         `
@@ -67,14 +73,8 @@ const loadPhones =  async(searchText) => {
 
 
   document.getElementById('btn-search').addEventListener('click', function(){
-    // start loader
-    tootleSpinner(true)
-
-    const searchField = document.getElementById('search-field');
-
-    const searchText = searchField.value;
  
-    loadPhones(searchText)
+    processSearch(10)
 
   })
 
@@ -93,5 +93,41 @@ const loadPhones =  async(searchText) => {
         loaderSection.classList.add('d-none')
      }
   }
+
+
+  document.getElementById('btn-show-all').addEventListener('click', function() {
+
+
+         processSearch();
+  })
+
+
+
+  const processSearch = (dataLimit) => {
+
+     tootleSpinner(true)
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    loadPhones(searchText, dataLimit)
+  }
+
+
+
+  const loadPhoneDetails = async id => {
+
+    const url = `https://openapi.programming-hero.com/api/phone/${id}`;
+
+    const res = await fetch(url);
+
+    const data = await res.json()
+
+    console.log(data)
+
+
+
+
+  }
+
+
 
 // loadPhones();
